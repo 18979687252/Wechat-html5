bf6aafa1d2939fd84971ef47e5182c565551908f
18979687252@163.com <18979687252@163.com>
2018-10-10 21:22:29 +0800

    完善

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -51,7 +51,7 @@
         name: 'Home',
         data() {
             return {
-                isLogin: 'Login'
+                isLogin: 'notLogin'
             }
         },
         methods: {
@@ -88,7 +88,7 @@
         created() {
             let sign = this.$route.query.sign
             localStorage['sign'] = sign
-//          localStorage['sign'] = 'ok3zx0QbILeQMMmT5Tn2YhPOZMC4'
+            // localStorage['sign'] = 'ok3zx0QbILeQMMmT5Tn2YhPOZMC4'
             this.chkAuto()
         }
     }
